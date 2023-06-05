76361a94db2f9aca6cbb2e32b0ce0951f8f8e4b1
VladKekw <wwlddss@gmail.com>
2023-06-05 08:06:28 +0300

    added consturctir

--- src/main/java/com/example/xcom/SektoLeader.java
+++ src/main/java/com/example/xcom/SektoLeader.java
@@ -1,4 +1,11 @@
 package com.example.xcom;
 
 public class SektoLeader extends SektoEngineer{
+    public final static int minhealth = 0;
+    public final static int maxhealth = 100;
+    private int health;
+    
+    public SektoLeader(int health){
+        this.health =health;
+    }
 }
